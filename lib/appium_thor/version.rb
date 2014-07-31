7e81af4e11192059ea75c1d3aee1c8378fdbe66a
bootstraponline <code@bootstraponline.com>
2014-07-31 10:56:13 -0400

    Release 0.0.2

--- lib/appium_thor/version.rb
+++ lib/appium_thor/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Thor
-    VERSION = '0.0.1' unless defined? ::Appium::Thor::VERSION
+    VERSION = '0.0.2' unless defined? ::Appium::Thor::VERSION
     DATE    = '2014-07-31' unless defined? ::Appium::Thor::DATE
   end
 end
