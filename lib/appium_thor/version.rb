31e6d7494e0402604e3ce8b10d426684e05c16a9
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2024-04-27 11:12:13 -0700

    Release 2.0.5

--- lib/appium_thor/version.rb
+++ lib/appium_thor/version.rb
@@ -1,6 +1,6 @@
 module Appium
   module Thor
-    VERSION = '2.0.4' unless defined? ::Appium::Thor::VERSION
-    DATE    = '2024-04-26' unless defined? ::Appium::Thor::DATE
+    VERSION = '2.0.5' unless defined? ::Appium::Thor::VERSION
+    DATE    = '2024-04-27' unless defined? ::Appium::Thor::DATE
   end
 end
